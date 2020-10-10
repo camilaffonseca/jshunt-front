55ae3d94c559bff38b35135c4ad767b1bc469c8b
Camila Fonseca <camilafonsecafp@outlook.com>
2020-10-10 20:10:24 -0300

    Migrate local repo to Github

---
+++ src/pages/main/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import api from '../../services/api'
+
+export default class Main extends Component {
+    state = {
+        products: []
+    }
+
+    componentDidMount() {
+        this.loadProducts()
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products')
+
+        this.setState({ products: response.data.docs })
+    }
+
+    render() {
+        return <h1>Length: {this.state.products.length}</h1>
+    }
+}
